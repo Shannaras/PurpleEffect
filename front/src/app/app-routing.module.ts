8277f8c9ce8ec6bc572ca405150e01984c46d92f
Shannaras <44708608+Shannaras@users.noreply.github.com>
2024-01-20 13:25:47 +0100

    add routing of contact, donation and join; add links to the navbar

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DonationComponent } from './components/donation/donation.component';
+import { JoinComponent } from './components/join/join.component';
 
 const routes: Routes = [
-  {path: 'about', component:AboutComponent}
+  {path: 'about', component:AboutComponent},
+  {path: 'contact', component:ContactComponent},
+  {path: 'donation', component:DonationComponent},
+  {path: 'join', component:JoinComponent},
 ];
 
 @NgModule({
