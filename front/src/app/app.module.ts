0bc5ba6cf148d11f4302d03f636fca48e496cde0
Shannaras <44708608+Shannaras@users.noreply.github.com>
2024-01-20 13:15:06 +0100

    add contact, donation and join components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { AboutComponent } from './components/about/about.component';
+import { JoinComponent } from './components/join/join.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DonationComponent } from './components/donation/donation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AboutComponent
+    AboutComponent,
+    JoinComponent,
+    ContactComponent,
+    DonationComponent,
   ],
   imports: [
     BrowserModule,
