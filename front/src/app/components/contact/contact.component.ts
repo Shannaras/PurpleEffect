e07beba7dd5f8f0656a8d2d22f9bff05cdacf680
Shannaras <44708608+Shannaras@users.noreply.github.com>
2024-01-21 01:18:10 +0100

    contact page implementation

--- front/src/app/components/contact/contact.component.ts
+++ front/src/app/components/contact/contact.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  public contactForm!: FormGroup;
 
+  constructor() { 
+    this.contactForm= new FormGroup({
+      copainDeLaNuit: new FormControl(false, Validators.required),
+      rdr: new FormControl(false, Validators.required),
+      safeZone: new FormControl(false, Validators.required),
+      jauge: new FormControl("", Validators.required),
+      lieu: new FormControl("", Validators.required),
+      nomEvent: new FormControl("", Validators.required),
+      typeEvent: new FormControl("", Validators.required),
+      email: new FormControl("", Validators.required)
+    })
+  } 
+
+  ngOnInit(){
+    
+  }
+  
+  onSubmit(){
+
+  }
 }
